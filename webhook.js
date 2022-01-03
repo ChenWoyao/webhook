11c8910aee79c207d27fe1967deaa8a7124ec44e
chenwoyao <chenwoyao@163.com>
2022-01-04 01:50:30 +0800

    test

--- webhook.js
+++ webhook.js
@@ -26,6 +26,7 @@ let server = http.createServer(function (req, res) {
       let event = req.headers['x-github-event'];
       //github传递了请求体body,同时传递了签名，需要验证签名是否正确
       let signatrue = req.headers['x-hub-signature'];
+      console.log('heders:', req.headers);
       console.log('signatrue:', signatrue);
       console.log('sign:', sign(body));
       console.log('event:', event);
